fff7cbdec010dc61d2c883746b10d1f36f46273b
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2025-04-29 12:24:23 +0700

    Add migration

---
+++ src/StudMart.PartnersMicroservice/Infrastructure/StudMart.PartnersMicroservice.Infrastructure.EntityFramework/Migrations/20250429045525_CategoriesTableMigration.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudMart.PartnersMicroservice.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoriesTableMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Partners_Category_CategoryId",
+                table: "Partners");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Categories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Category_Name",
+                table: "Categories",
+                newName: "IX_Categories_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Partners_Categories_CategoryId",
+                table: "Partners",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Partners_Categories_CategoryId",
+                table: "Partners");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Category");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Categories_Name",
+                table: "Category",
+                newName: "IX_Category_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Partners_Category_CategoryId",
+                table: "Partners",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
