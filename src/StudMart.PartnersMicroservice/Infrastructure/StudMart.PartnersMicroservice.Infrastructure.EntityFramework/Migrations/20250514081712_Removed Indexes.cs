ba52577c71f124d2c2067011a1f5848a1b6f6e09
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2025-05-14 15:39:25 +0700

    Migration added

---
+++ src/StudMart.PartnersMicroservice/Infrastructure/StudMart.PartnersMicroservice.Infrastructure.EntityFramework/Migrations/20250514081712_Removed Indexes.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudMart.PartnersMicroservice.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Partners_Email",
+                table: "Partners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Partners_Inn",
+                table: "Partners");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Partners_Phone",
+                table: "Partners");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Partners_Email",
+                table: "Partners",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partners_Inn",
+                table: "Partners",
+                column: "Inn",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partners_Phone",
+                table: "Partners",
+                column: "Phone",
+                unique: true);
+        }
+    }
+}
