bcb342f973407362e38476b9c5b6ba8da1dac615
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2025-05-27 19:26:18 +0700

    Работа с описаниями

---
+++ src/StudMart.PartnersMicroservice/Infrastructure/StudMart.PartnersMicroservice.Infrastructure.RabbitMq/Handlers/DescriptionRequestCreatedNotificationHandler.cs
@@ -0,0 +1,19 @@
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using StudMart.PartnersMicroservice.BusinessLogic.Models.DescriptionRequest;
+using StudMart.PartnersMicroservice.Infrastructure.RabbitMq.Handlers.Base;
+using StudMart.PartnersMicroservice.Infrastructure.RabbitMq.Helpers;
+using StudMart.PartnersMicroservice.Infrastructure.RabbitMq.Models;
+using StudMart.PartnersMicroservice.Infrastructure.RabbitMq.Notifications;
+
+namespace StudMart.PartnersMicroservice.Infrastructure.RabbitMq.Handlers;
+
+public class DescriptionRequestCreatedNotificationHandler(
+    IOptions<RabbitSettings> options,
+    IMapper mapper,
+    ILogger<INotificationHandler<DescriptionRequestCreatedNotification, DescriptionRequestModel>> logger)
+    : NotificationHandlerBase<DescriptionRequestCreatedNotification, DescriptionRequestModel,
+        DescriptionRequestServiceModel>(options, mapper, "descriptionRequestCreated", logger)
+{
+}
