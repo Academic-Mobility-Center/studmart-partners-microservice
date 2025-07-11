7d1d4040f5fb6693f7ec93fb9d42ba11ba940156
Vyacheslav Godunov <godunovslava@yandex.ru>
2025-07-11 12:06:22 +0300

    Добавил нотификацию кролика об удалении сотрудника партнера.

---
+++ src/StudMart.PartnersMicroservice/Infrastructure/StudMart.PartnersMicroservice.Infrastructure.RabbitMq/Handlers/DeleteEmployeeNotificationHandler.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using StudMart.PartnersMicroservice.BusinessLogic.Models.Employee;
+using StudMart.PartnersMicroservice.Infrastructure.RabbitMq.Handlers.Base;
+using StudMart.PartnersMicroservice.Infrastructure.RabbitMq.Helpers;
+using StudMart.PartnersMicroservice.Infrastructure.RabbitMq.Models;
+using StudMart.PartnersMicroservice.Infrastructure.RabbitMq.Notifications;
+
+namespace StudMart.PartnersMicroservice.Infrastructure.RabbitMq.Handlers;
+
+public class DeleteEmployeeNotificationHandler(
+    IOptions<RabbitSettings> options,
+    IMapper mapper,
+    ILogger<DeleteEmployeeNotificationHandler> logger) : NotificationHandlerBase<EmployeeDeletedNotification, EmployeeModel, EmployeeServiceModel>(options, mapper, "DeleteEmployee", logger)
+{
+    
+}
