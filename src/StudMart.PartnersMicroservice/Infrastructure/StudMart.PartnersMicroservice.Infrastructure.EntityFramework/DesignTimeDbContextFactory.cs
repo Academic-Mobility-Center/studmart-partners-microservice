741ccd77b3ae026a2ec12c05d59b93de03772be7
Vyacheslav Godunov <godunovslava@yandex.ru>
2025-07-30 01:33:27 +0300

    Добавил DesignTime фабрику, для формирования локальных миграций, при изменении модели.

---
+++ src/StudMart.PartnersMicroservice/Infrastructure/StudMart.PartnersMicroservice.Infrastructure.EntityFramework/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace StudMart.PartnersMicroservice.Infrastructure.EntityFramework;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        // Конфигурация для DesignTime (миграции)
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("designTimeAppSettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseNpgsql(
+            configuration.GetConnectionString("DefaultConnection"),
+            npgsqlOptions => npgsqlOptions.MigrationsAssembly("StudMart.PartnersMicroservice.Infrastructure.EntityFramework"));
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
